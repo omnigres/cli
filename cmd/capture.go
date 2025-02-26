fe7df984ac4103067d2bd473a39249ad79a619e8
Diogo Biazus <diogo@biazus.ca>
2025-02-26 17:12:11 -0500

    Problem: Connecting and running capture_schema_revision manually is too much trouble for an everyday task.
    Solution: Provide a CLI command capture that automates all the toil
    away. It connects to the orb database, calls the capture_schema_revision
    function with the proper paths (inferred from working directory) and
    remove the revision database once it finishes creating the revision
    files.

--- cmd/capture.go
+++ cmd/capture.go
@@ -19,10 +19,10 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var migrateCmd = &cobra.Command{
-	Use:   "migrate",
-	Short: "Migrate orbs",
-	Long:  `By default, will migrate all listed orbs`,
+var captureCmd = &cobra.Command{
+	Use:   "capture",
+	Short: "Capture orb schema changes",
+	Long:  `By default, will capture all listed orbs`,
 	Run: func(cmd *cobra.Command, args []string) {
 		var cluster orb.OrbCluster
 		var err error
@@ -43,14 +43,8 @@ var migrateCmd = &cobra.Command{
 		}
 
 		ctx := context.Background()
-		log.Debug("Migrating orbs", "orbs", orbs)
-		err = assembleOrbs(
-			ctx,
-			cluster,
-			dbReset,
-			orbs,
-			func(orbName string) string { return orbName },
-		)
+		log.Debug("Capturing orbs", "orbs", orbs)
+		captureOrbs(ctx, cluster, orbs)
 	},
 }
 
@@ -76,6 +70,89 @@ func currentOrbs(cluster orb.OrbCluster, dir string) ([]string, error) {
 	}
 
 	return currentOrbs(cluster, parent)
+}
+
+func captureSchemaRevision(
+	ctx context.Context,
+	cluster orb.OrbCluster,
+	orbName string,
+) (err error) {
+	var db *sql.DB
+	db, err = cluster.Connect(ctx, orbName)
+	if err != nil {
+		return
+	}
+
+	log.Infof("Capturing schema for orb %s", orbName)
+	_, err = db.ExecContext(ctx, "create extension if not exists omni_schema cascade")
+	if err != nil {
+		return err
+	}
+
+	jsonMessageReporting := func(notice *pq.Error) {
+		log.Infof("%s", notice.Message)
+	}
+	conn, err := db.Conn(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer conn.Close()
+	conn.Raw(func(driverConn any) error {
+		pq.SetNoticeHandler(driverConn.(driver.Conn), jsonMessageReporting)
+		return nil
+	})
+	var revision string
+	err = db.QueryRowContext(
+		ctx,
+		`select omni_schema.capture_schema_revision(omni_vfs.local_fs($1), 'src', 'revisions')`,
+		fmt.Sprintf("/mnt/host/%s", orbName),
+	).Scan(&revision)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	_, err = db.ExecContext(ctx, fmt.Sprintf("drop database \"%s\"", revision))
+	if err != nil {
+		log.Errorf("Could not remove revision. You can try to manually remove using DROP DATABASE %s", revision)
+	}
+	log.Infof("📦 Revision %s created", revision)
+
+	return
+}
+
+func captureOrbs(
+	ctx context.Context,
+	cluster orb.OrbCluster,
+	orbs []string,
+) (err error) {
+	var db *sql.DB
+	db, err = cluster.Connect(ctx, "omnigres")
+	if err != nil {
+		return
+	}
+	for _, orbName := range orbs {
+		log.Infof("Capturing orb %s", orbName)
+		var dbExists bool
+		err := db.QueryRowContext(
+			ctx,
+			`select exists(select from pg_database where datname = $1)`,
+			orbName,
+		).Scan(&dbExists)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		if !dbExists {
+			_, err = db.ExecContext(ctx, fmt.Sprintf(`create database %q`, orbName))
+			if err != nil {
+				log.Fatal(err)
+			}
+		}
+
+		captureSchemaRevision(ctx, cluster, orbName)
+	}
+	return
 }
 
 func assembleOrbs(
@@ -185,9 +262,6 @@ func assembleSchema(ctx context.Context, db *sql.DB, orbSource string, dbName st
 	}
 }
 
-var dbReset bool
-
 func init() {
-	rootCmd.AddCommand(migrateCmd)
-	migrateCmd.Flags().BoolVarP(&dbReset, "dbReset", "r", false, "dbReset")
+	rootCmd.AddCommand(captureCmd)
 }
